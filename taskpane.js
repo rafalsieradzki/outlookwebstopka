4f149957c964adfa080a714b9cf448b1d5da659d
rafalsieradzki <151756137+rafalsieradzki@users.noreply.github.com>
2026-05-21 11:10:46 +0200

    Add files via upload

--- taskpane.js
+++ taskpane.js
@@ -1,6 +1,5 @@
 // Stopka Familijna - taskpane.js
-// Poprawna wersja: uwierzytelnianie przez Office Dialog API.
-// Panel otwiera auth.html, auth.html zwraca access token przez Office.context.ui.messageParent().
+// Pełny HTML stopki + aktualna klauzula RODO + logowanie przez Office Dialog API + Microsoft Graph.
 
 const AUTH_URL = "https://rafalsieradzki.github.io/outlookwebstopka/auth.html";
 const GRAPH_ME_URL =
@@ -86,16 +85,6 @@ function getAccessTokenWithDialog() {
             }
           }
         );
-
-        authDialog.addEventHandler(
-          Office.EventType.DialogEventReceived,
-          function (arg) {
-            // 12006 bywa zwracane po zamknięciu okna; jeśli token już wrócił, ignorujemy.
-            if (arg && arg.error) {
-              console.warn("DialogEventReceived:", arg);
-            }
-          }
-        );
       }
     );
   });
@@ -129,6 +118,13 @@ function firstBusinessPhone(user) {
   return "";
 }
 
+function cleanupPhoneLine(html) {
+  return html
+    .replace(/tel\.\s*<span/g, '<span')
+    .replace(/kom\.\s*(<|$)/g, '$1')
+    .replace(/\s{2,}/g, " ");
+}
+
 function buildSignatureHtml(user) {
   const officeProfile = Office.context.mailbox.userProfile || {};
 
@@ -141,7 +137,7 @@ function buildSignatureHtml(user) {
   const officeLocation = user.officeLocation || "";
   const companyName = user.companyName || "";
 
-  let html = "<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"max-width:600px;font-family:Calibri, Arial;\">\n  <tr>\n    <td style=\"font-size:9pt;line-height:140%;color:#595959;border-left:3px solid #DF292F;padding-left:15px;\">\n      <span style=\"font-size:14pt;color:#DF292F;\">%%DisplayName%%</span><br />\n      <span>%%Title%%</span><br /><br />\n      <a href=\"mailto:%%Email%%\" style=\"color:#595959;text-decoration:none;\">%%Email%%</a><br />\n      <span style=\"color:#DF292F;\">tel.</span> %%PhoneNumber%%\n      <span style=\"color:#DF292F;\">kom.</span> %%MobileNumber%%\n    </td>\n  </tr>\n</table>";
+  let html = "\n<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"max-width:600px;font-family:Calibri, Arial;\">\n    <tr>\n        <td style=\"margin:auto;\" align=\"center\">\n            <img src=\"https://www.familijna.pl/uploads/drive/familijna_logotyp.png\" width=\"80%\" alt=\"GRUPA FAMILIJNA\" />\n        </td>\n        <td style=\"font-size:9pt;line-height:140%;color:#595959;border-left:3px solid #DF292F;padding-left:15px;\">\n            <span style=\"font-size:14pt;color:#DF292F;\">%%DisplayName%%</span>\n            <br />\n            <span>%%Title%%</span>\n            <br /><br />\n            <a href=\"https://familijna.pl\" style=\"color:#595959;text-decoration: none;\"><span style=\"color:#DF292F;\">www.</span>familijna.pl</a>\n            <span style=\"color:#DF292F;\">email:</span>\n            <a href=\"mailto:%%Email%%\" style=\"color:#595959;text-decoration: none;\">%%Email%%</a>\n            <br />\n            <span style=\"color:#DF292F;\">tel.</span> %%PhoneNumber%%\n            <span style=\"color:#DF292F;\">kom.</span> %%MobileNumber%%\n            <div style=\"padding-top:25px;\">\n                <a href=\"https://www.facebook.com/familijna\" style=\"display:inline-block;\"><img src=\"https://www.familijna.pl/uploads/drive/fb.png\" height=\"25\" width=\"25\" alt=\"facebook\" style=\"margin-right:5px;\" /></a>&nbsp;\n                <a href=\"https://www.instagram.com/familijna/\" style=\"display:inline-block;\"><img src=\"https://www.familijna.pl/uploads/drive/ig.png\" height=\"25\" width=\"25\" alt=\"instagram\" style=\"margin-right:5px;\" /></a>&nbsp;\n                <a href=\"https://m.me/familijna\" style=\"display:inline-block;\"><img src=\"https://www.familijna.pl/uploads/drive/ms.png\" height=\"25\" width=\"25\" alt=\"messenger\" style=\"margin-right:5px;\" /></a>&nbsp;\n                <a href=\"https://goo.gl/maps/kpEMXw6deUcjidot9\" style=\"display:inline-block;\"><img src=\"https://www.familijna.pl/uploads/drive/gm.png\" height=\"25\" width=\"25\" alt=\"google maps\" style=\"margin-right:5px;\" /></a>&nbsp;\n                <a href=\"https://www.youtube.com/@familijna1631/featured\" style=\"display:inline-block;\"><img src=\"https://www.familijna.pl/uploads/drive/yt.png\" height=\"25\" width=\"25\" alt=\"youtube\" style=\"margin-right:5px;\" /></a>&nbsp;\n                <a href=\"https://www.linkedin.com/company/familijna\" style=\"display:inline-block;\"><img src=\"https://www.familijna.pl/uploads/drive/in.png\" height=\"25\" width=\"25\" alt=\"linkedin\" style=\"margin-right:5px;\" /></a>&nbsp;\n            </div>\n        </td>\n    </tr>\n    <tr>\n        <td colspan=\"2\" style=\"padding-top:5px;font-size:7pt;line-height:120%;color:#595959;\">\n            <p><span style=\"color:#DF292F;\">GRUPA FAMILIJNA</span> Spółka z ograniczoną odpowiedzialnością, Kuźnica Czeszycka 11, 56-320 Krośnice, tel. 71 384 56 13</p>\n            <p>NIP: 9161351695, REGON: 020182505, BDO: 000084673.</p>\n            <br />\n            <p>Informacja dla odbiorcy: Informacje zawarte w niniejszym email-u oraz załącznikach do niego mają charakter poufny, są przeznaczone wyłącznie dla wskazanych adresatów. Jeśli nie są Państwo adresatem tego email-a, prosimy niezwłocznie o jego skasowanie oraz poinformowanie nadawcy. Wykonywanie kopii, ujawnienie, dystrybucja lub używanie niniejszego email-a do innych celów jest zabronione. Spółka Grupa Familijna Sp. z o.o. nie ponosi żadnej odpowiedzialności za zmiany email-a dokonane po jego wysłaniu.</p>\n            <p>Administratorem danych osobowych jest Grupa Familijna sp. z o.o. z siedzibą w Kuźnicy Czeszyckiej. Dane osobowe zawarte w korespondencji mailowej są przetwarzane w celu odpowiadania na pytania, dokonywania ustaleń, zawierania i realizacji umów z kontrahentami, rozpoznawania reklamacji, jak również ustalenia, dochodzenia i obrony roszczeń. Mają Państwo w szczególności prawo dostępu do swoich danych osobowych, żądania ich usunięcia i wniesienia sprzeciwu wobec przetwarzania danych. Szczegóły dotyczące przetwarzania danych osobowych i przysługujących praw znajdują się w <a href=\"https://www.grupafamilijna.pl/pl/polityka-prywatnosci\" style=\"color:#0645AD;text-decoration:underline;\">Polityce prywatności</a>.</p>\n        </td>\n    </tr>\n</table>\n";
 
   html = replaceAllSafe(html, "%%DisplayName%%", displayName);
   html = replaceAllSafe(html, "%%Email%%", email);
@@ -152,7 +148,7 @@ function buildSignatureHtml(user) {
   html = replaceAllSafe(html, "%%OfficeLocation%%", officeLocation);
   html = replaceAllSafe(html, "%%CompanyName%%", companyName);
 
-  return "<br><br>" + html;
+  return "<br><br>" + cleanupPhoneLine(html);
 }
 
 async function insertSignature() {
