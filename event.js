92c3c3201d696b0d14d8ee44aae24cf2a8bf5bfd
rafalsieradzki <151756137+rafalsieradzki@users.noreply.github.com>
2026-05-25 13:11:22 +0200

    Add files via upload

--- event.js
+++ event.js
@@ -1,20 +1,43 @@
-// event.js - prosty test eventu bez czerwonego tekstu
-console.log("EVENT JS SIMPLE TEST");
+// event.js - prosty test bez czerwonego tekstu, przez getAsync + setAsync
+// Ten wariant używa mechanizmu, który wcześniej zadziałał przy czerwonym teście.
+
+const SIMPLE_MARKER = 'data-gf-simple-test="1"';
 
 function onNewMessageComposeHandler(event) {
   try {
+    const insertHtml =
+      '<div data-gf-simple-test="1">Pozdrawiam,<br>__</div><br><br>';
 
-    const html =
-      'Pozdrawiam,<br>__<br><br>';
+    Office.context.mailbox.item.body.getAsync(
+      Office.CoercionType.Html,
+      { asyncContext: event },
+      function (getResult) {
+        if (getResult.status !== Office.AsyncResultStatus.Succeeded) {
+          getResult.asyncContext.completed();
+          return;
+        }
 
-    Office.context.mailbox.item.body.prependAsync(
-      html,
-      { coercionType: Office.CoercionType.Html },
-      function () {
-        event.completed();
+        const currentBody = getResult.value || "";
+
+        if (currentBody.indexOf(SIMPLE_MARKER) !== -1) {
+          getResult.asyncContext.completed();
+          return;
+        }
+
+        const newBody = insertHtml + currentBody;
+
+        Office.context.mailbox.item.body.setAsync(
+          newBody,
+          {
+            coercionType: Office.CoercionType.Html,
+            asyncContext: getResult.asyncContext
+          },
+          function () {
+            getResult.asyncContext.completed();
+          }
+        );
       }
     );
-
   } catch (e) {
     try { event.completed(); } catch (_) {}
   }
